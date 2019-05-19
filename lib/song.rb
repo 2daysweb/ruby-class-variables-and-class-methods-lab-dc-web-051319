c41f4069ffa5e4eb8a5983d7aaa100811c77c834
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-19 00:07:44 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,8 @@ attr_accessor :name, :artist, :genre, :artists, :genres, :count
 
 @@artists = []
 @@genres = []
-@@count = []
+@@count = 0
+@@songs = []
 
 def initialize(name, artist, genre)
     @name = name 
@@ -14,21 +15,33 @@ def initialize(name, artist, genre)
     
     @@artists << self.artist
     @@genres << self.genre
-  
-    @@count << self
+    @@songs << self 
+    @@count +=1
 end 
 
 def self.count
-  @@count.size
+  @@count
 end 
 
-def self.artists
-  @@artists
-  binding.pry
+def self.artist
+   binding.pry
+   @artist
 end 
 
 def self.genres
   @@genres
+end 
+
+def self.artists
+end 
+
+def self.genres 
+end 
+
+def self.genre_count
+end 
+
+def self.artist_count 
 end 
 
 end 
